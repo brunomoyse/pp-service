3f57453c8565e1609c62983b5c6edb050176c96c
Bruno Moyse <cloud@nuagemagique.dev>
2025-08-15 23:28:48 +0200

    Add ITM statistics and ROI tracking query

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -1,7 +1,18 @@
 INSERT INTO clubs (id, name, city, country)
 VALUES
+    (gen_random_uuid(), 'Phenix Poker Club', 'Sambreville', 'BE'),
+    (gen_random_uuid(), 'Poker Family', 'Spa', 'BE'),
+    (gen_random_uuid(), 'PCDA', 'Spa', 'BE'),
+    (gen_random_uuid(), 'Tournai Poker Club', 'Tournai', 'BE'),
+    (gen_random_uuid(), 'Brussels Poker Club', 'Brussels', 'BE'),
+    (gen_random_uuid(), 'Poker One', 'Charleroi', 'BE'),
+    (gen_random_uuid(), 'Poker Nation', 'Ronse', 'BE'),
+    (gen_random_uuid(), 'Poker Club Limburg', 'Hasselt', 'BE'),
+    (gen_random_uuid(), 'Jumet Poker Club', 'Jumet', 'BE'),
+    (gen_random_uuid(), 'Peace Poker', 'Namur', 'BE'),
+    (gen_random_uuid(), 'Team PCDC', 'Lessines', 'BE'),
     (gen_random_uuid(), 'Liège Poker Club', 'Liège', 'BE'),
-    (gen_random_uuid(), 'Pokah Room', 'Antwerpen', 'BE');
+    (gen_random_uuid(), 'Pokah Room Antwerp', 'Antwerp', 'BE');
 
 WITH c AS (
     SELECT id FROM clubs WHERE name = 'Liège Poker Club' LIMIT 1
